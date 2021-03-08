1a77547d6220a0c95a1805dd01aed8add6ae0497
lyo-n <1y0mmmm@gmail.com>
2021-03-08 22:38:42 +0200

    hw-01

---
+++ nodejs-hw-01/contacts.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const contactsPath = path.join('db', 'contacts.json');
+
+
+function listContacts() {
+    fs.readFile(contactsPath, {encoding: 'utf-8'}, (err, data) =>{
+        try {
+            const parsedData = JSON.parse(data);
+            console.table(parsedData);
+        }
+        catch (error){
+            console.log('listContacts error:', err);
+        }
+    });
+  }
+  
+  function getContactById(contactId) {
+    fs.readFile(contactsPath, {encoding: 'utf-8'}, (err, data) =>{
+        try {
+            const parsedData = JSON.parse(data);
+            const findContactsById = parsedData.find(item => item.id === contactId);
+            console.log('Contact by id:', findContactsById)
+        }
+        catch(error) {
+            console.log('getContactById error:', err);
+        }        
+     });
+  }
+  
+  function removeContact(contactId) {
+    fs.readFile(contactsPath, { encoding: 'utf-8' }, (err, data) => {
+      try {
+        const parsedData = JSON.parse(data);
+        const removeContactById = parsedData.filter(item => item.id !== contactId);
+  
+        console.table(removeContactById);
+        fs.writeFile(contactsPath, JSON.stringify(removeContactById), () => {
+          console.log('Contact was removed');
+        });
+        return removeContactById;
+      } catch (error) {
+        console.log('remuveContact error:', err);
+      }
+    });
+  }
+  
+  function addContact(name, email, phone) {
+        fs.readFile(contactsPath, {encoding: 'utf-8'},(err, data) => {
+            try {
+                const parsedData = JSON.parse(data);
+                const newContact = {name, email, phone};
+                parsedData.push(newContact);
+                fs.writeFile(contactsPath, JSON.stringify(parsedData), () => {
+                    console.log('New contact added');
+                });
+            }
+            catch(error) {
+                console.log('Added error:', err);
+            }
+        });
+  }
+
+  module.exports = {listContacts, getContactById, removeContact, addContact};
